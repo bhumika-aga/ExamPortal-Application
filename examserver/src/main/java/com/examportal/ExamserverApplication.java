317a37f804c0deffe7698c155da2e84ebb7b689e
Bhumika Agarwal <bhumika.aga@gmail.com>
2023-02-22 15:27:02 +0530

    Finished Project

--- examserver/src/main/java/com/examportal/ExamserverApplication.java
+++ examserver/src/main/java/com/examportal/ExamserverApplication.java
@@ -1,17 +1,11 @@
 package com.examportal;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.examportal.model.Role;
-import com.examportal.model.User;
-import com.examportal.model.UserRole;
 import com.examportal.service.UserService;
 
 @SpringBootApplication
@@ -31,26 +25,26 @@ public class ExamserverApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("Starting Exam Portal Application!");
 
-		User user = new User();
-		user.setFirstName("Bhumika");
-		user.setLastName("Agarwal");
-		user.setUsername("bhumika");
-		user.setPassword(passwordEncoder.encode("bhumika"));
-		user.setEmail("bhumika.aga@gmail.com");
-		user.setPhone("9876543210");
-		user.setProfile("default.png");
-
-		Role role = new Role();
-		role.setRoleId(44L);
-		role.setRoleName("ADMIN");
-
-		UserRole userRole = new UserRole();
-		userRole.setRole(role);
-		userRole.setUser(user);
-		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(userRole);
-
-		User user1 = userService.createUser(user, userRoles);
-		System.out.println(user1);
+//		User user = new User();
+//		user.setFirstName("Bhumika");
+//		user.setLastName("Agarwal");
+//		user.setUsername("bhumika");
+//		user.setPassword(passwordEncoder.encode("bhumika"));
+//		user.setEmail("bhumika.aga@gmail.com");
+//		user.setPhone("9876543210");
+//		user.setProfile("default.png");
+//
+//		Role role = new Role();
+//		role.setRoleId(44L);
+//		role.setRoleName("ADMIN");
+//
+//		UserRole userRole = new UserRole();
+//		userRole.setRole(role);
+//		userRole.setUser(user);
+//		Set<UserRole> userRoles = new HashSet<>();
+//		userRoles.add(userRole);
+//
+//		User user1 = userService.createUser(user, userRoles);
+//		System.out.println(user1);
 	}
 }
